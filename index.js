a119da2908439fd4ccbc9b37a3ed52a8a58710f9
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-02-22 13:45:41 +0100

    progress bar done

--- index.js
+++ index.js
@@ -22,3 +22,23 @@ const navLinks = document.getElementById("navLinks");
 burguer.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+//progress bar
+document.addEventListener("DOMContentLoaded", () => {
+  const coloredBar = document.getElementById("coloredBar");
+
+  window.addEventListener(
+    "scroll",
+    (calculatePercentage = () => {
+      let h = document.documentElement;
+
+      let st = h.scrollTop;
+      let sh = h.scrollHeight;
+      let ch = h.clientHeight;
+
+      let percent = Math.round((st / (sh - ch)) * 100);
+
+      coloredBar.style.width = percent + "%";
+    })
+  );
+});
