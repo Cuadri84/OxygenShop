d7e1edc0e4497c2981e2c83e889c7ee6d8707827
Cuadri84 <davidcuadrillerosaura@gmail.com>
2023-02-23 17:33:31 +0100

    Done Form Post to JSONPlaceholder.com

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//GO Up Button
+//GO UP BUTTON
 const goUpWithTimer = () => {
   setTimeout(goUp, 200);
 };
@@ -14,7 +14,7 @@ const goUp = () => {
   });
 };
 
-//hamburguer menu
+//HAMBURGUER MENU
 
 const burguer = document.getElementById("burguer");
 const navLinks = document.getElementById("navLinks");
@@ -23,7 +23,8 @@ burguer.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-//progress bar
+//PROGRESS BAR
+
 document.addEventListener("DOMContentLoaded", () => {
   const coloredBar = document.getElementById("coloredBar");
 
@@ -43,27 +44,61 @@ document.addEventListener("DOMContentLoaded", () => {
   );
 });
 
-//form validation
+//FORM VALIDATION AND FETCH
+
 const nameValue = document.getElementById("nameValue");
 const mail = document.getElementById("mail");
 const consent = document.getElementById("consent");
 const form = document.getElementById("form");
+let nameOK = "";
+const url = "https://jsonplaceholder.typicode.com/posts";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const nameValueLength = e.target.nameValue.value.length;
   const mailValue = e.target.mail.value;
+  const checked = e.target.consent.checked;
   const expReg =
     /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+  if (
+    checked === true &&
+    expReg.test(mailValue) &&
+    (nameValueLength >= 1 || nameValueLength <= 100)
+  ) {
+    nameOK = e.target.nameValue.value;
+    sendUser(url, nameOK, mailValue);
+  }
 
   if (nameValueLength <= 1 || nameValueLength >= 100) {
     document.getElementById("nameValue").style.borderStyle = "solid";
     document.getElementById("nameValue").style.borderColor = "red";
   }
-  if (expReg.test(mailValue)) {
-    console.log("correcto");
-  } else {
+
+  if (!expReg.test(mailValue)) {
     document.getElementById("mail").style.borderStyle = "solid";
     document.getElementById("mail").style.borderColor = "red";
   }
+
+  if (checked === false) {
+    document.getElementById("consent").style.outline = "2px solid #c00";
+  }
 });
+
+function sendUser(url, nameOK, mailOK) {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify({
+      name: nameOK,
+      email: mailOK,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
+
+//MODAL
